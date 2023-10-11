2bdfc56ba92518f40de65e91ce636ec9bbc763f4
PaxtonLocal <144478764+PaxtonCardinal@users.noreply.github.com>
2023-10-11 13:56:52 -0600

    Added Model

--- develop/ticketmaster.js
+++ develop/ticketmaster.js
@@ -240,7 +240,7 @@ function fetchEventData(enteredZipcode, enteredMileRange, selectedCategoryId) {
           addMarkersToMap(map, eventInfoArray, selectedCategoryId);
         } else {
           console.log('No events found for the given postal code.');
-          alert('No events found for the given postal code.');
+          openModal(document.getElementById('custom-modal'));
         }
     })
 
@@ -269,6 +269,46 @@ window.initMap = initMap;
 document.addEventListener("DOMContentLoaded", function () {
   initMap();
 });
+
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+      closeAllModals();
+    }
+  });
 
 // Assume you have a saved ZIP code
 var savedZipcode = "12345"; // Replace with your actual saved ZIP code
